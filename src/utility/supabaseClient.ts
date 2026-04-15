aae1bb5df66322027d2e41785ba0f9cb69b1fc0f
Misa Ravololonantenaina <55345000+Misa100@users.noreply.github.com>
2026-04-15 18:51:38 +0300

    feat: workflow en local

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,9 +1,7 @@
 import { createClient } from "@refinedev/supabase";
 
-const SUPABASE_URL = "https://vajfrtovfusygoljujoe.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhamZydG92ZnVzeWdvbGp1am9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1ODYwNzAsImV4cCI6MjA3MjE2MjA3MH0.uNco8lSNFO287RzoS5CRyk8yvvg5VEbXqFFSf-TWrLg";
-
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "http://127.0.0.1:54321";
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || "sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH";
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {
